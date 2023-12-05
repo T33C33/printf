1082707b81424c3b4a413e4b69b47a5e6f7252e8
T33C33 <teeceeiheukwumere@gmail.com>
2023-12-05 06:17:53 -0800

    betty fixes

--- test_zero.c
+++ test_zero.c
@@ -5,19 +5,15 @@
  *@format: is a character string
  *
  *Return: 0
- *
  */
-
 int _printf(const char *format, ...)
 {
 	int jantee_count = 0, length = 0;
 	va_list argument_list;
+	char character, *string;
 
 	if (format == NULL)
-	{
 		return (-1);
-	}
-
 	va_start(argument_list, format);
 
 	while (*format)
@@ -31,9 +27,7 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			if (*format == '\0')
-			{
 				break;
-			}
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -41,18 +35,16 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 'c')
 			{
-				char character = va_arg(argument_list, int);
+				character = va_arg(argument_list, int);
 				write(1, &character, 1);
 				jantee_count++;
 			}
 			else if (*format == 's')
 			{
-				char *string = va_arg(argument_list, char*);
+				string = va_arg(argument_list, char*);
 				while (string[length] != '\0')
-				{
 					length++;
-				}
-				write (1, string, length);
+				write(1, string, length);
 				jantee_count += length;
 			}
 		}
