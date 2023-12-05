5763fb459188c88422db6575b4ab5ded9cc03d0c
Janice Gathoga <janicegathoga@gmail.com>
2023-12-05 09:58:49 -0800

    test0-solo version

---
+++ printf_0.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf -produces output format
+ * @format: is a character srting
+ * Return: no of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list argument_list;
+	int jantee_count = 0;
+
+	va_start(argument_list, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(argument_list, int);
+
+				jantee_count += write(1, &c, 1);
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(argument_list, char *);
+
+				jantee_count += write(1, s, strlen(s));
+			}
+			else if (*format == '%')
+			{
+				jantee_count += write(1, "%", 1);
+			}
+		}
+		else
+		{
+			jantee_count += write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(argument_list);
+	return (jantee_count);
+}
+
