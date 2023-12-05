e3d1852e2ac647ede1a66a5e4871b4c50e37d2c0
Janice Gathoga <janicegathoga@gmail.com>
2023-12-05 13:32:50 -0800

    modified task

---
+++ print_function.h
@@ -0,0 +1,10 @@
+#ifndef PRINT_FUNCTIONS_H
+#define PRINT_FUNCTIONS_H
+
+#include <stdarg.h>
+
+int print_char(va_list list);
+int print_string(va_list list);
+int print_int(va_list list);
+
+#endif
